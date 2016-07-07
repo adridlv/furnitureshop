a70187cf434c5726973737c0c0e1f7ef6f71adc2
Paco Tiger <Paco Tiger>
2016-07-07 10:18:14 +0200

    creando vistas 2 día

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 var app = angular.module('shop',[]);//en el array inyectamos dependencias
+
+app.config(['$routeProvider',function($routeProvider) {
+    $routeProvider
+    .when("/",{
+        templateUrl: "views/home.html",
+        controller: "homeManager"
+    })
+    .otherwise({
+        redirectTo: "/"
+    })
+}]);
+
 app.controller('furnituresManager',['$scope','$http', function($scope,$http){
     
     $http.get("json/furnitures.json").success (function (data){
         $scope.furnitures = data;
     });
+}]);
+
+app.controller('homeManager', ['$scope', function($scope){
+
+
 }]);
