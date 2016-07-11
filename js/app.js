96b326e8f773163092d18d0c1a41850f0781e4b2
Paco Tiger <Paco Tiger>
2016-07-11 19:50:06 +0200

    Se añaden las sillas al catalogo, y mejora de UX con efectos especiales en la intro

--- js/app.js
+++ js/app.js
@@ -60,6 +60,39 @@ app.controller("productManager",["$scope", "$http", "$routeParams", function($sc
 
 app.controller('homeManager', ['$scope',"Home", function($scope, home){
 	home.initializeSlider();
+	$scope.eventoSofa = "images/sofas/i1.jpg";
+	$scope.eventoBed = "images/bed/i1.jpg";
+	$scope.eventoTable = "images/table/i1.jpg";
+	$scope.eventoChair = "images/chair/i1.jpg";
+
+
+	$scope.mouseOnSofa = function() {
+		$scope.eventoSofa = "images/sofas/i2.jpg";
+	}
+	$scope.mouseOnBed = function() {
+		$scope.eventoBed = "images/bed/i2.jpg";
+	}
+	$scope.mouseOnTable = function() {
+		$scope.eventoTable = "images/table/i2.jpg";
+	}
+	$scope.mouseOnChair = function() {
+		$scope.eventoChair = "images/chair/i2.jpg";
+	}
+
+
+	$scope.mouseOffSofa = function() {
+		$scope.eventoSofa = "images/sofas/i1.jpg";
+	}
+	$scope.mouseOffBed = function() {
+		$scope.eventoBed = "images/bed/i1.jpg";
+	}
+	$scope.mouseOffTable = function() {
+		$scope.eventoTable = "images/table/i1.jpg";
+	}
+	$scope.mouseOffChair = function() {
+		$scope.eventoChair = "images/chair/i1.jpg";
+	}
+
 }]);
 
 app.controller("contactManager", ["$scope", function(){
